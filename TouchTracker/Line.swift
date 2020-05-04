7bf2b9a23dc7267b0421be66e49fdd106c1f1a80
Shalom Yemane <yemaneshalom@gmail.com>
2020-05-04 01:18:04 -0700

    first commit

---
+++ TouchTracker/Line.swift
@@ -0,0 +1,15 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by CSUFTitan on 5/3/20.
+//  Copyright © 2020 CSUFTitan. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Line {
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+}
---
+++ TouchTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TouchTracker
+//
+//  Created by CSUFTitan on 5/3/20.
+//  Copyright © 2020 CSUFTitan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
